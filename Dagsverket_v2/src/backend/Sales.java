ad2ab88208c797df93468f81d947576f8f6d20d6
Bjørn Hoxmark <bhoxmark@gmail.com>
2014-03-27 11:16:58 +0100

    Sales updated with updateList

--- Dagsverket_v2/src/backend/Sales.java
+++ Dagsverket_v2/src/backend/Sales.java
@@ -7,14 +7,14 @@
 package backend;
 
 import java.util.*;
+import java.sql.*;
 
-/**
- *
- * @author Jorgen
- */
+
+
 public class Sales {
     private ArrayList<Sale> sales;
-
+    private Database db;
+    
     public Sales() {
     	this.sales = new ArrayList<Sale>();
     }
@@ -24,10 +24,38 @@ public class Sales {
     }
 
     public boolean createSale() {
+        
+        
     	return false;
     }
 
     public void updateSaleList() {
-    	// update sales
+        String sqlStatement = "Select  sales.ID, sales.QUANTITY, customers.FIRSTNAME," +
+        "customers.LASTNAME, customers.PHONENUMBER," +
+        "customers.ADDRESS, customers.POSTALCODE, customers.POSTPLACE," +
+        "wood.WOODTYPE, wood.BAGSIZE, wood.PRICE" +
+        "from sales NATURAL JOIN wood, customers;";
+        ResultSet rs = db.executeQuery(sqlStatement);
+        try {
+            while(rs.next()) {
+                sales.add(new Sale(
+                    rs.getInt("sales.ID"),
+                    rs.getInt("sales.QUANTITY"),                   
+                    rs.getString("customers.FIRSTNAME"),
+                    rs.getString("customers.LASTNAME"),
+                    rs.getInt("customers.PHONENUMBER"),
+                    rs.getString("customers.ADDRESS"),
+                    rs.getInt("customers.POSTALCODE"),
+                    rs.getString("customers.POSTPLACE"),
+                    rs.getString("wood.WOODTYPE"),
+                    rs.getInt("wood.BAGSIZE"),                    
+                    rs.getInt("wood.PRICE")                     
+                    ));
+            }
+        } catch(SQLException e) {
+            System.out.println("SQLError: " + e);
+        } finally {
+            db.closeAll();
+        }
     }
 }
