5bfec7e58f0f651ae6c6ffa330d5a969aaca77da
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2014-04-09 18:57:47 +0200

    Lagd klasse URLGenerator.

---
+++ Dagsverket_v2/src/backend/URLGenerator.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package backend;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+/**
+ *
+ * @author Jorgen
+ */
+public class URLGenerator {
+    public static void generateMapURL(String address, int postalCode, String postPlace){
+        try{
+            String[] addressArray = address.split(" ");
+            String addressURL = "http://www.google.no/maps/dir/Hornebergveien+5,+7038+Trondheim/";
+            for(int i = 0; i<addressArray.length; i++){
+                if(i<addressArray.length-1){
+                    addressURL+= addressArray[i] + "+";
+                }
+                else{
+                    addressURL += addressArray[i];
+                }
+            }
+            addressURL += ",";
+            String postalCodeString = "+" + postalCode;
+            addressURL += postalCodeString;
+            addressURL += "+";
+            addressURL += postPlace;
+            addressURL+="/";
+            addressURL = addressURL.replaceAll("æ", "ae");
+            addressURL = addressURL.replaceAll("ø", "ae");
+            addressURL = addressURL.replaceAll("å", "ae");
+            addressURL = addressURL.replaceAll("Æ", "AE");
+            addressURL = addressURL.replaceAll("Ø", "O");
+            addressURL = addressURL.replaceAll("Å", "AA");
+            try {
+                Desktop d = Desktop.getDesktop();
+                d.browse(new URI(addressURL));
+            } catch(IOException | URISyntaxException ex) {
+                ex.printStackTrace();
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            showMessageDialog(null, "Vennligst velg et oppdrag");
+        }
+    }         
+}
--- Dagsverket_v2/src/frontend/ViewActiveTab.java
+++ Dagsverket_v2/src/frontend/ViewActiveTab.java
@@ -224,37 +224,10 @@ public class ViewActiveTab extends javax.swing.JPanel {
     }//GEN-LAST:event_buttonAddEmployee1ActionPerformed
 
     private void buttonDrivingRouteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonDrivingRouteActionPerformed
-        try{
+         try{
             int id = (int)jTable2.getValueAt(jTable2.getSelectedRow(), 0);
-            String address = this.cases.getCaseById(id).getAddress();
-            String[] addressArray = address.split(" ");
-            String addressURL = "http://www.google.no/maps/dir/Hornebergveien+5,+7038+Trondheim/";
-            for(int i = 0; i<addressArray.length; i++){
-                if(i<addressArray.length-1){
-                    addressURL+= addressArray[i] + "+";
-                }
-                else{
-                    addressURL += addressArray[i];
-                }
-            }
-            addressURL += ",";
-            String postalCode = "+"+this.cases.getCaseById(id).getPostalCode();
-            addressURL += postalCode;
-            addressURL += "+";
-            addressURL += this.cases.getCaseById(id).getPostPlace();
-            addressURL+="/";
-            addressURL = addressURL.replaceAll("æ", "ae");
-            addressURL = addressURL.replaceAll("ø", "ae");
-            addressURL = addressURL.replaceAll("å", "ae");
-            addressURL = addressURL.replaceAll("Æ", "AE");
-            addressURL = addressURL.replaceAll("Ø", "O");
-            addressURL = addressURL.replaceAll("Å", "AA");
-            try {
-                Desktop d = Desktop.getDesktop();
-                d.browse(new URI(addressURL));
-            } catch(IOException | URISyntaxException ex) {
-                ex.printStackTrace();
-            }
+            URLGenerator.generateMapURL(this.cases.getCaseById(id).getAddress(), this.cases.getCaseById(id).getPostalCode(),
+                                    this.cases.getCaseById(id).getPostPlace());
         }catch(ArrayIndexOutOfBoundsException e){
             showMessageDialog(null, "Vennligst velg et oppdrag");
         }
